99a5a9dbe779556128a5b3ffd7f5ce086fd470d6
ayly8 <allison.y.ly@gmail.com>
2024-11-02 19:24:12 -0700

    started setting up firebase and database, user registration functionality in progress

--- my-book-rec/src/components/UserLogin.jsx
+++ my-book-rec/src/components/UserLogin.jsx
@@ -1,10 +1,29 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { auth } from './firebase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import '../css/userlogin.css';
 import user_icon from '../assets/user.png'
 import pass_icon from '../assets/padlock.png'
 
 // this component creates the user login component
 function UserLogin() {
+   // State variables to store username and password
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   const [error, setError] = useState('');
+
+   const handleLogin = async () => {
+      const auth = getAuth();
+      try {
+         // Firebase authentication method
+         await signInWithEmailAndPassword(auth, username, password);
+         console.log('User logged in successfully');
+      } catch (err) {
+         setError('Invalid username or password');
+      }
+   };
+
    return (
       <div id="login-container">
          <div id="login-header">
@@ -13,19 +32,30 @@ function UserLogin() {
          <div id="login-info">
             <div className="login-inputs">
                <img src = {user_icon} alt = "user icon" />
-               <input type = "text" placeholder="Username"/>
+               <input 
+                  type = "text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+               />
             </div>
             <div className="login-inputs">
                <img src = {pass_icon} alt = "password lock icon" />
-               <input type = "password" placeholder="Password"/>
+               <input
+                  type = "password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+               />
             </div>
          </div>
+         {error && <div className="error-message">{error}</div>}
          <div id="reg-link">
             Don't have an account yet?{' '}
             <Link to="/register">Register here!</Link>
          </div>
          <div id="login-submit-container">
-            <div className="submit">Login</div>
+            <div className="submit" onClick={handleLogin}>Login</div>
          </div>
       </div>
    );
--- my-book-rec/src/components/UserRegister.jsx
+++ my-book-rec/src/components/UserRegister.jsx
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { auth, db} from './firebase';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { setDoc, doc } from "firebase/firestore";
+
 import '../css/userregister.css';
 import user_icon from '../assets/user.png'
 import email_icon from '../assets/email.png'
@@ -6,6 +11,44 @@ import pass_icon from '../assets/padlock.png'
 
 // this component creates the user register component
 function UserRegister() {
+   const [username, setUsername] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [error, setError] = useState('');
+
+   const handleRegister = async () => {
+      const auth = getAuth();
+      try {
+         await createUserWithEmailAndPassword(auth, email, password);
+         console.log('User registered successfully');
+         const user = auth.currentUser;
+         // If user is created successfully, store additional information in Firestore
+         if (user) {
+            await setDoc(doc(db, 'Users', user.uid), {
+               username: username,
+               email: user.email
+            });
+            console.log('User registered and data stored in Firestore');
+         }
+      } catch (err) {
+         // Customize error messages based on Firebase error codes
+         switch (err.code) {
+            case 'auth/invalid-email':
+               setError('Please enter a valid email address.');
+               break;
+            case 'auth/weak-password':
+               setError('Password should be at least 6 characters.');
+               break;
+            case 'auth/email-already-in-use':
+               setError('This email is already registered. Please log in or use another email.');
+               break;
+            default:
+               setError('Registration failed. Please try again.');
+               break;
+         } 
+      }
+   };
+
    return (
       <div id="reg-container">
          <div id="reg-header">
@@ -14,23 +57,39 @@ function UserRegister() {
          <div id="reg-info">
             <div className="reg-inputs">
                <img src = {user_icon} alt = "user icon" />
-               <input type = "text" placeholder="Username"/>
+               <input
+                  type = "text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+               />
             </div>
             <div className="reg-inputs">
                <img src = {email_icon} alt = "email icon" />
-               <input type = "email" placeholder="Email"/>
+               <input
+                  type = "email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+               />
             </div>
             <div className="reg-inputs">
                <img src = {pass_icon} alt = "password lock icon" />
-               <input type = "password" placeholder="Password"/>
+               <input
+                  type = "password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+               />
             </div>
          </div>
+         {error && <div className="error-message">{error}</div>}
          <div id="login-link">
             Already have an account?{' '}
             <Link to="/login">Login here!</Link>
          </div>
          <div id="reg-submit-container">
-            <div className="submit">Sign Up</div>
+            <div className="submit" onClick={handleRegister}>Sign Up</div>
          </div>
       </div>
    );
