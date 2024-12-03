dfbf901a473a5f61b42fdfba773ee55ee8e2f4b1
ayly8 <allison.y.ly@gmail.com>
2024-12-03 14:43:17 -0800

    fixed weird issue with survey on home.jsx

--- my-book-rec/src/pages/Home.jsx
+++ my-book-rec/src/pages/Home.jsx
@@ -1,26 +1,9 @@
-// import React from 'react';
-// import Survey from "../components/Survey"
-// import '../css/home.css';
-
-// function Home() {
-
-//    return (
-//       <>
-//          <Survey />
-//          <button id="submit-btn">Submit</button>
-//          <p id="chatGPT-reponse"></p>
-//       </>
-//    )
-// }
-
-// export default Home
-
 import React, { useState } from 'react';
 import Survey from "../components/Survey";
 import '../css/home.css';
 
 function Home() {
-   const [recommendation, setRecommendation] = useState('');
+   const [recommendation, setRecommendation] = useState(null);
 
    const handleSurveySubmit = (response) => {
       setRecommendation(response); // Update the recommendation state with the API response
@@ -29,7 +12,6 @@ function Home() {
    return (
       <div className="home-container">
          <Survey onSubmit={handleSurveySubmit} />
-         {recommendation && <p id="chatGPT-reponse">{recommendation}</p>} {/* Show the recommendation if it exists */}
       </div>
    );
 }
